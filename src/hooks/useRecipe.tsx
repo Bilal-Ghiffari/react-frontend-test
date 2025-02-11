62b552e19f7c8aa127ff5138a62bbbd3cece28ea
Bilal-Ghiffari <bilalalghiffari53@gmail.com>
2025-02-12 00:58:41 +0700

    feat(recipe): add custom hooks recipe page

---
+++ src/hooks/useRecipe.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { ICoffee } from "../type";
+import { formatRupiah } from "../utils";
+import { getListOfIngredients } from "../services/localStorage";
+
+const useRecipe = () => {
+  const [recipeIngredients, setRecipeIngredients] = React.useState<ICoffee[]>(
+    []
+  );
+  const [cupCount, setCupCount] = React.useState<number>(1);
+
+  React.useEffect(() => {
+    setRecipeIngredients(getListOfIngredients());
+  }, []);
+
+  const handleSubmit = (values: { cups: number }) => {
+    setCupCount(values.cups);
+  };
+
+  const totalCOGS = recipeIngredients.reduce((total, ingredient) => {
+    return total + ingredient.qty * cupCount * ingredient.total_price;
+  }, 0);
+
+  const ingredientLists = recipeIngredients.map((coffee) => ({
+    ingredient: coffee.name,
+    amounte: `${coffee.qty * cupCount} ${coffee?.uom?.toUpperCase()}`,
+    total_cost: formatRupiah(coffee.qty * cupCount * coffee.total_price),
+  }));
+
+  return {
+    recipeIngredients,
+    cupCount,
+    ingredientLists,
+    totalCOGS,
+    setCupCount,
+    handleSubmit,
+  };
+};
+
+export default useRecipe;
