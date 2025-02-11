707b4b0d6821d89d875b5646d4bf7c7a351e46a6
Bilal-Ghiffari <bilalalghiffari53@gmail.com>
2025-02-12 00:32:55 +0700

    feat(wrapper table): add component wrapper table

---
+++ src/components/wrapper-table.tsx
@@ -0,0 +1,40 @@
+import { SizeType } from "antd/es/config-provider/SizeContext";
+import * as React from "react";
+import CommonTable from "./common/common-table";
+
+interface IWrapperTableProps<T> {
+  tableData: T[];
+  tableConfig: {
+    columns: Array<any>;
+    rowKey: string;
+    size?: SizeType;
+  };
+  isShowPagination?: boolean;
+  pagination?: React.ReactNode;
+  positionPagination?: "top" | "bottom" | "both";
+  loading: boolean;
+}
+
+const WrapperTable = <T extends {}>({
+  tableData,
+  tableConfig,
+  isShowPagination = false,
+  pagination,
+  positionPagination,
+  loading,
+}: IWrapperTableProps<T>) => {
+  const getTableConfig = () => ({
+    columns: tableConfig.columns,
+    dataSource: tableData,
+    rowKey: tableConfig.rowKey || "key",
+    size: tableConfig.size,
+  });
+  return (
+    <section className="bg-[#ffffff] px-6 py-8 rounded-2xl">
+      <CommonTable {...getTableConfig()} loading={loading} />
+      {isShowPagination && <div className="mt-10">{pagination}</div>}
+    </section>
+  );
+};
+
+export default WrapperTable;
